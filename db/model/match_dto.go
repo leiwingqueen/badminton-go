ad5184c12b1e614c3fc303eb850059e51e888420
leiwingqueen <leiwingqueen@qq.com>
2022-01-02 11:42:34 +0800

    bugfix

--- db/model/match_dto.go
+++ db/model/match_dto.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type MatchDTO struct {
-	MatchId   int64     `gorm:"column:matchId" json:"id"`
-	Name      string    `gorm:"column:name" json:"name"`
-	StartAt   time.Time `gorm:"column:startAt" json:"startAt"`
-	CreatedAt time.Time `gorm:"column:createdAt" json:"createdAt"`
-	UpdatedAt time.Time `gorm:"column:updatedAt" json:"updatedAt"`
-	Status    int32     `gorm:"column:status" json:"status"`
+	MatchId  int64     `gorm:"column:matchId" json:"id"`
+	Name     string    `gorm:"column:name" json:"name"`
+	StartAt  time.Time `gorm:"column:startAt" json:"startAt"`
+	CreateAt time.Time `gorm:"column:createAt" json:"createAt"`
+	UpdateAt time.Time `gorm:"column:updateAt" json:"updateAt"`
+	Status   int32     `gorm:"column:status" json:"status"`
 }
